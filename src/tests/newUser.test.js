b79b5fba9be984f09e93e9ec4f614403d8b680ce
David Rafael <dd.castellar@gmail.com>
2021-10-01 22:47:21 -0500

    Test all fields in the creation of new users.

--- src/tests/newUser.test.js
+++ src/tests/newUser.test.js
@@ -30,11 +30,120 @@ describe('Test for the creation of new users.', () =>
             })
     })
 
-    // it('It should return the user previously created.', async (done) => 
-    // {
-        
-    // })
+    it('It should fail to create a new user due to invalid name (numbers on it).',
+    (done) => 
+    {
+        const newUser = 
+        {
+            name : 'Test Test 85',
+            username: 'test',
+            email: 'test@test.com',
+            password: 'testPassword',
+            phone: 5555555
+        }
+
+        chai.request(app)
+            .post('/users/register')
+            .send(newUser)
+            .end((err, response) => 
+            {
+                response.should.have.status(400)
+                response.should.be.an('object')
+                done()
+            })
+    })
+
+    it('It should fail to create a new user due to invalid username (less than 3 characters).', 
+    (done) => 
+    {
+        const newUser = 
+        {
+            name : 'Test Test',
+            username: 'te',
+            email: 'test@test.com',
+            password: 'testPassword',
+            phone: 5555555
+        }
+
+        chai.request(app)
+            .post('/users/register')
+            .send(newUser)
+            .end((err, response) => 
+            {
+                response.should.have.status(400)
+                response.should.be.an('object')
+                done()
+            })
+    })
     
+    it('It should fail to create a new user due to duplicate email.', (done) => 
+    {
+        const newUser = 
+        {
+            name : 'Test Test',
+            username: 'test',
+            email: 'test@test.com',
+            password: 'testPassword',
+            phone: 5555555
+        }
+
+        chai.request(app)
+            .post('/users/register')
+            .send(newUser)
+            .end((err, response) => 
+            {
+                response.should.have.status(400)
+                response.should.be.an('object')
+                done()
+            })
+    })
+
+    it('It should fail to create a new user due to invalid password (less than 6 characters).', 
+    (done) => 
+    {
+        const newUser = 
+        {
+            name : 'Test Test',
+            username: 'test',
+            email: 'test@test.com',
+            password: 'Pass',
+            phone: 5555555
+        }
+
+        chai.request(app)
+            .post('/users/register')
+            .send(newUser)
+            .end((err, response) => 
+            {
+                response.should.have.status(400)
+                response.should.be.an('object')
+                done()
+            })
+    })
+
+    it('It should fail to create a new user due to invalid phone (characters on it).', 
+    (done) => 
+    {
+        const newUser = 
+        {
+            name : 'Test Test',
+            username: 'test',
+            email: 'test@test.com',
+            password: 'testPassword',
+            phone: '+5555555'
+        }
+
+        chai.request(app)
+            .post('/users/register')
+            .send(newUser)
+            .end((err, response) => 
+            {
+                response.should.have.status(400)
+                response.should.be.an('object')
+                done()
+            })
+    })
+
     after(async () => 
     {
         await User.deleteOne({email: "test@test.com"})
